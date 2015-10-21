b8f37c5ae31b2b4e4a8b1ae582d06977afeabad7
softwarespot <softwarespotapps@gmail.com>
2015-10-21 21:09:19 +0300

    Renamed API

--- js/gists.js
+++ js/gists.js
@@ -8,16 +8,18 @@
  */
 App.namespace().gists = (function (window, document, $, core, undefined) {
     // Constants
-    var API = {
-        getGistsByUser: 'https://api.github.com/users/{username}/gists',
-        getGistById: 'https://api.github.com/gists/{id}'
-    };
 
     // SemVer version number of the module
     var VERSION = '1.0.0';
 
     // Unique global identifier. Internal usage only
     // var GUID = 'EBF09F7D-008B-4B56-8227-0B5378649968';
+
+    // API resource URIs
+    var _api = {
+        GET_GISTS_BY_USER: 'https://api.github.com/users/{username}/gists',
+        GET_GIST_BY_ID: 'https://api.github.com/gists/{id}'
+    };
 
     // Fields
 
@@ -99,7 +101,7 @@ App.namespace().gists = (function (window, document, $, core, undefined) {
     function _load(username) {
         var options = {
             // Replace the '{username}' with the user's username
-            url: API.getGistsByUser.replace('{username}', username),
+            url: _api.GET_GISTS_BY_USER.replace('{username}', username),
             method: 'get',
             dataType: 'jsonp',
             cache: false
--- js/github.js
+++ js/github.js
@@ -11,15 +11,17 @@
  */
 App.namespace().github = (function (window, document, $, core, undefined) {
     // Constants
-    var API = {
-        getGitHubByUser: 'https://api.github.com/users/{username}'
-    };
 
     // SemVer version number of the module
     var VERSION = '1.0.0';
 
     // Unique global identifier. Internal usage only
     // var GUID = 'EBF09F7D-008B-4B56-8227-0B5378649968';
+
+    // API resource URIs
+    var _api = {
+        GET_GITHUB_BY_USER: 'https://api.github.com/users/{username}'
+    };
 
     // Fields
 
@@ -100,7 +102,7 @@ App.namespace().github = (function (window, document, $, core, undefined) {
     function _load(username) {
         var options = {
             // Replace the '{username}' with the user's username
-            url: API.getGitHubByUser.replace('{username}', username),
+            url: _api.GET_GITHUB_BY_USER.replace('{username}', username),
             method: 'get',
             dataType: 'jsonp',
             cache: false
