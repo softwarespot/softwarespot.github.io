6931c36257bae10c385f1dd9001eeb5cb35d0cf6
softwarespot <softwarespotapps@gmail.com>
2015-11-29 18:39:27 +0200

    Moved built files to build directory

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,12 @@ var uglify = require('gulp-uglify');
 var del = require('del');
 
 // See the cssmin documentation for more details
-var cssMinSettings = {
+var _cssMinSettings = {
     keepSpecialComments: 0,
 };
 
 // See the uglify documentation for more details
-var uglifySettings = {
+var _uglifySettings = {
     compress: {
         comparisons: true,
         conditionals: true,
@@ -32,13 +32,12 @@ var uglifySettings = {
 // Assets for the project
 var Assets = {
     css: {
-        dest: 'css',
+        dest: './build/css',
+        source: './css',
         custom: {
             all: [
 
-                // Note: /css/ is the same as Assets.css.dest
-
-                // Select all js file(s) include sub-directories
+                // Select all css file(s) include sub-directories
                 './css/**/*.css',
 
                 // Ignore all css file(s) that have the .min.css prefix
@@ -53,12 +52,18 @@ var Assets = {
             minified: 'vendor.min.css',
         },
     },
+    fonts: {
+        dest: './build/fonts',
+    },
+    images: {
+        dest: './build/images',
+        source: './images/**/*.*',
+    },
     js: {
-        dest: 'js',
+        dest: './build/js',
+        source: './js',
         custom: {
             all: [
-
-                // Note: /js/ is the same as Assets.js.dest
 
                 // Select all js file(s) include sub-directories
                 './js/**/*.js',
@@ -78,94 +83,120 @@ var Assets = {
 };
 
 // Clean the current directory
-gulp.task('clean', function(cb) {
+gulp.task('clean', function cleanTask(cb) {
     del([
-        './' + Assets.css.dest + '/' + Assets.css.custom.minified,
-        './' + Assets.js.dest + '/' + Assets.js.custom.minified,
+        Assets.css.dest + '/**/*.*',
+        Assets.images.dest + '/**/*.*',
+        Assets.js.dest + '/**/*.*',
     ], cb);
 });
 
 // Minify the main css file(s)
-gulp.task('cssmin', function() {
+gulp.task('cssmin', function cssMinTask() {
     // Store the destination directory
-    var dest = './' + Assets.css.dest;
+    var dest = Assets.css.dest;
 
-    // Clean the css dest directory
-    del(['./' + Assets.css.dest + '/' + Assets.css.custom.minified]);
+    // Store the source directory
+    var source = Assets.css.source;
+
+    // Store the minified filename
+    var minified = Assets.css.custom.minified;
+
+    // Clean the css destination directory
+    del([dest + '/' + minified]);
 
     return gulp.src([
-            dest + '/styles.css',
+            source + '/styles.css',
         ])
-        .pipe(concat(Assets.css.custom.minified))
-        .pipe(cssmin(cssMinSettings))
-        .pipe(rename(Assets.css.custom.minified))
-        .pipe(gulp.dest('./' + Assets.css.dest));
+        .pipe(concat(minified))
+        .pipe(cssmin(_cssMinSettings))
+        .pipe(rename(minified))
+        .pipe(gulp.dest(dest));
+});
+
+// Copy images to the destination directory
+gulp.task('images', function imagesTask() {
+    // Copy images to the destination directory
+    return gulp.src(Assets.images.source)
+        .pipe(gulp.dest(Assets.images.dest));
 });
 
 // Check the code meets the following standards outlined in .jshintrc
-gulp.task('jshint', function() {
+gulp.task('jshint', function jsHintTask() {
     return gulp.src(Assets.js.custom.all)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Initialise the PHP server 'php -S localhost:8000'
-gulp.task('php-server', function() {
+gulp.task('php-server', function phpSeverTask() {
     php.server({
         hostname: 'localhost',
         port: 8000,
         base: '.',
         keepalive: true,
-        open: true
+        open: true,
 
-        // bin: ''
-        // ini: ''
+        // bin: '',
+        // ini: '',
     });
 });
 
 // Prettify the main js file(s)
-gulp.task('prettify-js', function() {
+gulp.task('prettify-js', function prettifyJSTask() {
     gulp.src(Assets.js.custom.all)
         .pipe(prettify({
             config: '.jsbeautifyrc',
             mode: 'VERIFY_AND_WRITE',
         }))
-        .pipe(gulp.dest('./' + Assets.js.dest));
+        .pipe(gulp.dest('./' + Assets.js.source));
 });
 
 // Uglify aka minify the main js file(s)
-gulp.task('uglify', function() {
+gulp.task('uglify', function uglifyTask() {
     // Store the destination directory
-    var dest = './' + Assets.js.dest;
+    var dest = Assets.js.dest;
 
-    // Clean the js dest directory
-    del([dest + '/' + Assets.js.custom.minified]);
+    // Store the source directory
+    var source = Assets.js.source;
+
+    // Store the minified filename
+    var minified = Assets.js.custom.minified;
+
+    // Clean the js destination directory
+    del([dest + '/' + minified]);
 
     return gulp.src([
-            dest + '/core.js',
-            dest + '/core.api.js',
-            dest + '/core.features.js',
-            dest + '/gists.js',
-            dest + '/main.js',
-            dest + '/navigation.js',
+            source + '/core.js',
+            source + '/core.api.js',
+            source + '/core.features.js',
+            source + '/gists.js',
+            source + '/main.js',
+            source + '/navigation.js',
         ])
-        .pipe(concat(Assets.js.custom.minified))
-        .pipe(uglify(uglifySettings))
-        .pipe(rename(Assets.js.custom.minified))
+        .pipe(concat(minified))
+        .pipe(uglify(_uglifySettings))
+        .pipe(rename(minified))
         .pipe(gulp.dest(dest));
 });
 
 // Concat and uglify the vendor scripts/styles
-gulp.task('vendor', function() {
+gulp.task('vendor', function vendorTask() {
     // Store the bower_components directory
     var bowerComponents = './bower_components/';
 
-    // Copy fonts
+    // Copy fonts to the destination directory
     gulp.src([
             bowerComponents + 'font-awesome/fonts/**/*.{eof,svg,ttf,woff,woff2}',
             bowerComponents + 'open-sans/fonts/**/*.{eof,svg,ttf,woff,woff2}',
         ])
-        .pipe(gulp.dest('./fonts'));
+        .pipe(gulp.dest(Assets.fonts.dest));
+
+    // Store the css destination directory
+    var cssDest = Assets.css.dest;
+
+    // Store the css minified filename
+    var cssMinified = Assets.css.vendor.minified;
 
     // Concatenate and minify styles
     gulp.src([
@@ -175,9 +206,15 @@ gulp.task('vendor', function() {
             bowerComponents + 'skeleton/css/skeleton.css',
             bowerComponents + 'nprogress/nprogress.css',
         ])
-        .pipe(concat(Assets.css.vendor.minified))
-        .pipe(cssmin(cssMinSettings))
-        .pipe(gulp.dest('./' + Assets.css.dest));
+        .pipe(concat(cssMinified))
+        .pipe(cssmin(_cssMinSettings))
+        .pipe(gulp.dest(cssDest));
+
+    // Store the js destination directory
+    var jsDest = Assets.js.dest;
+
+    // Store the js minified filename
+    var jsMinified = Assets.js.vendor.minified;
 
     // Concatenate and uglify scripts
     gulp.src([
@@ -187,25 +224,26 @@ gulp.task('vendor', function() {
             bowerComponents + 'jquery-handlebars/jquery-handlebars.js',
             bowerComponents + 'nprogress/nprogress.js',
         ])
-        .pipe(concat(Assets.js.vendor.minified))
-        .pipe(uglify(uglifySettings))
-        .pipe(gulp.dest('./' + Assets.js.dest));
+        .pipe(concat(jsMinified))
+        .pipe(uglify(_uglifySettings))
+        .pipe(gulp.dest(jsDest));
 });
 
 // Build the main css and js file(s)
-gulp.task('build', ['clean', 'cssmin', 'uglify']);
+gulp.task('build', ['cssmin', 'uglify']);
 
 // Watch for changes to the main css and js file(s)
-gulp.task('watch', function() {
+gulp.task('watch', function watchTask() {
     gulp.watch(Assets.css.custom.all, ['cssmin']);
     gulp.watch(Assets.js.custom.all, ['jshint', 'uglify']);
 });
 
 // Register the default task which is essentially 'build' and 'vendor' included
-gulp.task('default', ['build', 'vendor']);
+gulp.task('default', ['clean', 'build', 'images', 'vendor']);
 
 // 'gulp build' to build the main css and js file(s)
 // 'gulp cssmin' to minify the main css file(s)
+// 'gulp images' to copy images files to the destination directory
 // 'gulp jshint' to check the syntax of the main js file(s)
 // 'gulp php-server' to connect to the local PHP server
 // 'gulp prettify-js' to prettify the main js file(s)
